1aee98685ffa476c0b5b2b96d7e533d35f8a9374
techsolutioncat <steven.penzeve130@gmail.com>
2024-02-28 15:45:25 -0500

    Finish header

--- src/components/auth/header/Header.js
+++ src/components/auth/header/Header.js
@@ -1,11 +1,21 @@
 
 import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faNavicon, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 import Button from '../../common/button/Button'
 import './style.scss'
 import logo from '../../../assets/images/logo.svg'
 import MobileLogo from '../../../assets/images/mobile-logo.svg'
 
 function Header() {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setShowMobileMenu(!showMobileMenu);
+    };
+
     return (
         <header className='header-main__wrapper'>
             <div className='header-main__container d-flex'>
@@ -13,13 +23,24 @@ function Header() {
                     <Link to="/" className='logo'>
                         <img src={logo} alt='logo'></img>
                     </Link>
-                    <Link to="/" className='mobile-logo'>
+                    <Link to="/" className='mobile-logo d-hide'>
                         <img src={MobileLogo} alt='logo'></img>
                     </Link>
                 </div>
                 <div className='site-navigation'>
-                    <Button data={{ link: '/login', label: 'Log in', LinkClass: 'nav-link link-login btn-transparent font-Syne fw-500 br-10 br-10 td-none'}}></Button>
-                    <Button data={{ link: '/', label: 'Sign Up', LinkClass: 'nav-link link-signup btn-gradient font-Syne fw-700 br-10 br-10 td-none'}}></Button>
+                    <Button data={{ link: '/login', label: 'Log in', LinkClass: 'nav-link link-login btn-transparent font-Syne fw-500 br-10 br-10 td-none' }}></Button>
+                    <Button data={{ link: '/', label: 'Sign Up', LinkClass: 'nav-link link-signup btn-gradient font-Syne fw-700 br-10 br-10 td-none' }}></Button>
+                    <div className='mobile-menu d-hide'>
+                        {showMobileMenu ? <FontAwesomeIcon icon={faXmark} className='close-menu' onClick={toggleMobileMenu} />: <FontAwesomeIcon icon={faNavicon} className='open-menu' onClick={toggleMobileMenu} /> }
+                        {showMobileMenu ? <ul className="menu-list ls-none p-0 br-10">
+                            <li>
+                                <Link to="/login" className='mo-nav-link font-Syne td-none'>Log in</Link>
+                            </li>
+                            <li>
+                                <Link to="/" className='mo-nav-link font-Syne td-none'>Sign Up</Link>
+                            </li>
+                        </ul>: ''}
+                    </div>
                 </div>
             </div>
         </header>
--- src/components/common/button/Button.js
+++ src/components/common/button/Button.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import './style.scss'
 
 function Button(props) {
     const { link, label, LinkClass } = props.data;
